a7a4e8327d83127dcd46b1d9d6e7b9dd0588df46
Em <emilylauder@gmail.com>
2021-01-10 15:07:30 -0500

    Updated portfolio images and added .js file

---
+++ script.js
@@ -0,0 +1,27 @@
+function displayKaren() {
+  window.open("https://emkaygru.github.io/AskKaren/", "_blank");
+}
+
+function showMemeMaker() {
+  window.open("https://github.com/emkaygru/Meme-Maker-2077", "_blank");
+}
+
+function showHerokuLinkMeme() {
+  window.open("https://meme-maker-2077.herokuapp.com/", "_blank");
+}
+
+function showNoteTaker() {
+  window.open("https://github.com/emkaygru/NoteTaker", "_blank");
+}
+
+function showHerokuLinkNote() {
+  window.open("https://emkaygru-notetaker.herokuapp.com/", "_blank");
+}
+
+function showPortfolio() {
+  window.open("portfolio.html", "_blank");
+}
+
+function contact() {
+  window.open("contact.html", "_blank");
+}
